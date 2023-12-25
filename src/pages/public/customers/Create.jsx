889c4c876fdcc8b9c93aa7fb56d241079290781c
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2023-12-25 13:15:40 +0100

    Refactorizando

--- src/pages/public/customers/Create.jsx
+++ src/pages/public/customers/Create.jsx
@@ -1,5 +1,44 @@
 export default function Create() {
     return (
-        <h2>Create</h2>
+        <section>
+            <h2>Create New Customer Accopunt</h2>
+            <form>
+                <div>
+                    <label>
+                        <span>Fisrst Name*</span>
+                        <input type="text" autoComplete="name" required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Last Name*</span>
+                        <input type="text" autoComplete="lastname" required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Email*</span>
+                        <input type="Email" autoComplete="Email" required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Password*</span>
+                        <input type="password" autoComplete="password" required />
+                    </label>
+                    <label>
+                        <span>Confirm Password*</span>
+                        <input type="password" autoComplete="password" required />
+                    </label>
+                    <label className="showpassword">
+                        <input type="checkbox" name="showpassword" />
+                        <span>Show Password</span>
+                    </label>
+                </div>
+                <div>
+                    <button type="submit">Create Account</button>
+                </div>
+            </form>
+        </section>
     )
 }
