fbd6e366945b3542e2c64edc3a2d2761320f8ae3
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2023-12-27 10:17:11 +0100

    Commit

--- src/pages/public/view-cart/ViewCart.jsx
+++ src/pages/public/view-cart/ViewCart.jsx
@@ -1,5 +1,29 @@
+import { Link } from 'react-router-dom'
+import styles from './css/style.module.css'
 export default function ViewCart() {
     return (
-        <h2>Shopping Cart</h2>
+        <section className={styles.containerShopping}>
+            <h2 className={styles.titlePage}>Shopping Cart</h2>
+            <section>
+                <span>Items</span>
+                <ul>
+                    <article>
+                        <section className={styles.containerLeft}>
+                            <div className={styles.containerImg}>
+                                <img src="" alt="" />
+                            </div>
+                            <div>
+                                <Link to="">Título de la imagen</Link>
+                            </div>
+                        </section>
+                        <section className={styles.containerLeft}>
+                            Summary
+                        </section>
+                    </article>
+                </ul>
+            </section>
+
+        </section>
     )
 }
+
